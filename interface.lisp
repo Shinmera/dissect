a283815d45f68323bd6bcfd6ba3488f26eb2d3a7
Shinmera <shinmera@tymoon.eu>
2015-02-27 19:40:06 +0100

    Fix for SBCL's eager optimisation wherein it says that RESTARTS/STACK return NULL in the PRESENT method due to the definition being compiled and loaded before the implementation specific override. Adding an FTYPE declaration to ensure that a list is always returned fixes this.

--- interface.lisp
+++ interface.lisp
@@ -44,35 +44,42 @@
     (format stream "[~a] ~a~@[ | ~a~@[:~a~]~]"
             (pos call) (call call) (file call) (line call))))
 
+(declaim (ftype (function () list) stack))
 (defun stack ())
 
+(declaim (ftype (function () list) restarts))
 (defun restarts ())
 
-(defgeneric present (thing &optional stream)
-  (:method ((condition condition) &optional (stream T))
-    (format stream "~a" condition)
-    (format stream "~&   [Condition of type ~s]" (type-of condition))
-    (format stream "~&~%")
-    (present T stream))
-  (:method ((thing (eql T)) &optional (stream T))
-    (present (restarts) stream)
-    (format stream "~&~%")
-    (present (stack) stream))
-  (:method ((list list) &optional (stream T))
-    (when list
-      (etypecase (first list)
-        (restart (format stream "~&Available restarts:")
-         (loop for i from 0
-               for item in list
-               do (format stream "~& ~d: " i)
-                  (present item stream)))
-        (call (format stream "~&Backtrace:")
-         (loop for item in list
-               do (format stream "~& ")
-                  (present item stream))))))
-  (:method ((restart restart) &optional (stream T))
-    (format stream "[~a] ~a" (name restart) (report restart)))
-  (:method ((call call) &optional (stream T))
-    (let ((*print-pretty* NIL))
-      (format stream "~d: ~:[~s ~s~;(~s~{ ~s~})~]"
-              (pos call) (listp (args call)) (call call) (args call)))))
+(defgeneric present (thing &optional stream))
+
+(defmethod present ((condition condition) &optional (stream T))
+  (format stream "~a" condition)
+  (format stream "~&   [Condition of type ~s]" (type-of condition))
+  (format stream "~&~%")
+  (present T stream))
+
+(defmethod present ((thing (eql T)) &optional (stream T))
+  (present (restarts) stream)
+  (format stream "~&~%")
+  (present (stack) stream))
+
+(defmethod present ((list list) &optional (stream T))
+  (when list
+    (etypecase (first list)
+      (restart (format stream "~&Available restarts:")
+       (loop for i from 0
+             for item in list
+             do (format stream "~& ~d: " i)
+                (present item stream)))
+      (call (format stream "~&Backtrace:")
+       (loop for item in list
+             do (format stream "~& ")
+                (present item stream))))))
+
+(defmethod present ((restart restart) &optional (stream T))
+  (format stream "[~a] ~a" (name restart) (report restart)))
+
+(defmethod present ((call call) &optional (stream T))
+  (let ((*print-pretty* NIL))
+    (format stream "~d: ~:[~s ~s~;(~s~{ ~s~})~]"
+            (pos call) (listp (args call)) (call call) (args call))))
